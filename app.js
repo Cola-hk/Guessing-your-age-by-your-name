c194667ff89b3d53220f114351a1882416de8b39
Cola <99972634+Cola-hk@users.noreply.github.com>
2022-04-07 09:01:24 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const textAge = document.getElementById("textAge");
+const textInfo = document.getElementById("textInfo");
+const inputBox = document.getElementById("userInput")
+
+textLabel.textContent = ""
+
+function main() {
+    textInfo.textContent = "You are probably...";
+    let name = inputBox.value; 
+    fetch('https://api.agify.io?name='+name)
+    .then(response => response.json())
+    .then(data => {
+    let age = data['age'];
+
+    textAge.textContent = age;
+
+    if (age == null){
+        textInfo.textContent = "Error ! Please try again";
+        textAge.textContent = ""
+    }
+    })
+}
